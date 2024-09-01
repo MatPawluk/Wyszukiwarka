c1e65b29aa62715080a1e4734354bb7cabe00e04
Mateusz Pawluk <pawluk1988@icloud.com>
2024-09-01 13:50:39 +0200

    coments remove

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-/*
-co pobrać:
-    input
-    ul list
-    li
-
-Zastanów się jakich elementów będziesz potrzebował/a 
-(jakie elementy będziesz musiał/a pobrać za pomocą np. querySelectora).
-
-Stwórz funkcję, która będzie się odpalała, kiedy wpiszemy coś do inputa.
-
-Funkcja ma porównywać to, co wpisaliśmy w inputa z każdym napojem na liście.
-
-Za pomocą instrukcji warunkowej sprawdź, czy dane z inputa pokrywają się z nazwą napoju. 
-Jeśli nie, ukryj taki napój.
-
-Żeby aplikacja działała poprawnie, musimy się zabezpieczyć. 
-Użytkownik może podać nazwę napoju dużymi lub małymi literami. 
-Musimy więc sprowadzić to, co wpisuje użytkownik do np. małych liter 
-(tak samo trzeba zrobić z listą napojów).
-*/
-
 const input = document.querySelector(".search");
 const drinkNames = document.querySelectorAll("li");
 
